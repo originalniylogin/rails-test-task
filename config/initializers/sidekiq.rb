a3069cf014655f8dbd66df026e2536233048055e
originalniylogin <il007770@gmail.com>
2018-02-18 18:17:49 +0300

    Еще разок подготовил для деплоя на heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: "app3_sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV["REDIS_URL"], namespace: "app3_sidekiq_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: "app3_sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV["REDIS_URL"], namespace: "app3_sidekiq_#{Rails.env}" }
 end
 
 Sidekiq::Extensions.enable_delay!
